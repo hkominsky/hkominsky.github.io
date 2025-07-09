45256aef7b01a02b1d4a1e8973f4c91c9df75f6f
Hugh <hugh.kominsky@gmail.com>
2025-07-09 09:20:51 -0400

    Turned perks ffa page into a template for editing later

--- scripts/project-data/perks-ffa.js
+++ scripts/project-data/perks-ffa.js
@@ -1,21 +1,18 @@
 export default {
-  title: "Pickaxe Knockout",
+  title: "Perks FFA",
   description:
-    "A fast-paced multiplayer indie game where mining meets combat.",
+    "x",
   heroImage: {
-    src: "/projects/images/pickaxe-knockout/pickaxe-knockout.png",
-    alt: "Pickaxe Knockout"
+    src: "/projects/images/perks-ffa/perks-ffa.png",
+    alt: "Perks FFA"
   },
   stats: [
-    { number: "1,200+", label: "Unique Players" },
-    { number: "28 Minute", label: "Average Retention" }
+    { number: "x", label: "x" },
+    { number: "x", label: "x" }
   ],
   overview: [
-    "Independetly developed and deployed, the game features custom arenas, "
-    + "unique cosmetics, and over <strong>30 distinct power-ups</strong> designed around "
-    + "pickaxe-based combat.",
-    "The game balances competitive mechanics and rewards with laid-back enjoyment, keeping "
-    + "the experience both rewarding and fun."
+    "x",
+    "x"
   ],
   techStack: [
     "Unreal Engine 5", 
@@ -25,92 +22,54 @@ export default {
   ],
   gallery: [
     {
-      src: "/projects/images/pickaxe-knockout/map.png",
-      alt: "Arena",
-      caption: "Custom Map"
+      src: "/projects/images/perks-ffa/perks-ffa.png",
+      alt: "x",
+      caption: "x"
     },
     {
-      src: "/projects/images/pickaxe-knockout/blender.png",
-      alt: "Blender",
-      caption: "Asset Creation"
+      src: "/projects/images/perks-ffa/perks-ffa.png",
+      alt: "x",
+      caption: "x"
     },
     {
-      src: "/projects/images/pickaxe-knockout/uefn.png",
-      alt: "UEFN Editor",
-      caption: "Developing World"
+      src: "/projects/images/perks-ffa/perks-ffa.png",
+      alt: "x",
+      caption: "x"
     },
     {
-      src: "/projects/images/pickaxe-knockout/code.png",
-      alt: "Code",
-      caption: "Programming Mechanics"
+      src: "/projects/images/perks-ffa/perks-ffa.png",
+      alt: "x",
+      caption: "x"
     }
   ],
   timeline: [
-    { date: "Dec. 2024", description: "Built the game and lobby islands." },
-    { date: "Jan. 2025", description: "Brainstormed and coded 30+ custom power-ups." },
-    { date: "Feb. 2025", description: "Implemented an xp based rank system." },
-    { date: "Mar. 2025", description: "Designed the game's thumbnail and advertisement images." },
-    { date: "Mar. 2025", description: "Modeled custom 3D assets and cosmetics." },
-    { date: "Apr. 2025", description: "Finalized icons for each unique power-up." },
-    { date: "Apr. 2025", description: "Conducted internal testing and iterative polish with playtesters." },
-    { date: "June 2025", description: "Final deployment and release of the game." }
+    { date: "x", description: "x" },
   ],
   outcomes: {
     intro: [
-      "As my first large-scale independent project, it was an invaluable learning experience.",
-      "Here's what stood out:"
+      "x",
+      "x"
     ],
     cards: [
       {
-        icon: `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-            xmlns="http://www.w3.org/2000/svg" stroke="currentColor"
-            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M12 21L11.8999 20.8499C11.2053 19.808 10.858 19.287 10.3991 18.9098C9.99286 18.5759 9.52476 18.3254 9.02161 18.1726C8.45325 18 7.82711 18 6.57482 18H5.2C4.07989 18 3.51984 18 3.09202 17.782C2.71569 17.5903 2.40973 17.2843 2.21799 16.908C2 16.4802 2 15.9201 2 14.8V6.2C2 5.07989 2 4.51984 2.21799 4.09202C2.40973 3.71569 2.71569 3.40973 3.09202 3.21799C3.51984 3 4.07989 3 5.2 3H5.6C7.84021 3 8.96031 3 9.81596 3.43597C10.5686 3.81947 11.1805 4.43139 11.564 5.18404C12 6.03968 12 7.15979 12 9.4M12 21V9.4M12 21L12.1001 20.8499C12.7947 19.808 13.142 19.287 13.6009 18.9098C14.0071 18.5759 14.4752 18.3254 14.9784 18.1726C15.5467 18 16.1729 18 17.4252 18H18.8C19.9201 18 20.4802 18 20.908 17.782C21.2843 17.5903 21.5903 17.2843 21.782 16.908C22 16.4802 22 15.9201 22 14.8V6.2C22 5.07989 22 4.51984 21.782 4.09202C21.5903 3.71569 21.2843 3.40973 20.908 3.21799C20.4802 3 19.9201 3 18.8 3H18.4C16.1598 3 15.0397 3 14.184 3.43597C13.4314 3.81947 12.8195 4.43139 12.436 5.18404C12 6.03968 12 7.15979 12 9.4"/>
-        </svg>
-        `,
-        title: "Technical Adaptation",
-        description:
-          "Needed to quickly learn new technologies like and Unreal "
-          + "Engine and Verse by studying UEFN documentation."
+        icon: "x",
+        title: "x",
+        description: "x"
       },
       {
-        icon: `
-        <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" fill="none"/>
-            <path d="M12 6v6l4 2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-        </svg>
-        `,
-        title: "Long-Term Planning",
-        description:
-          "Committed to months of solo development, staying "
-          + "focused without external deadlines or structure."
+        icon: "x",
+        title: "x",
+        description: "x"
       },
       {
-        icon: `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" 
-            xmlns="http://www.w3.org/2000/svg" stroke="currentColor" 
-            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M22 7L14.1314 14.8686C13.7354 15.2646 13.5373 15.4627 13.309 15.5368C13.1082 15.6021 12.8918 15.6021 12.691 15.5368C12.4627 15.4627 12.2646 15.2646 11.8686 14.8686L9.13137 12.1314C8.73535 11.7354 8.53735 11.5373 8.30902 11.4632C8.10817 11.3979 7.89183 11.3979 7.69098 11.4632C7.46265 11.5373 7.26465 11.7354 6.86863 12.1314L2 17M22 7H15M22 7V14"/>
-        </svg>
-        `,
-        title: "Iterative Designing",
-        description:
-          "Refined gameplay through frequent play testing and market "
-          + "research to create an engaging experience for users."
+        icon: "x",
+        title: "x",
+        description:"x"
       },
       {
-        icon: `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" 
-            xmlns="http://www.w3.org/2000/svg" stroke="currentColor" 
-            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M10 17.6586V20C10 21.1046 10.8954 22 12 22C13.1046 22 14 21.1046 14 20V17.6586M12 2V3M3 12H2M5.5 5.5L4.8999 4.8999M18.5 5.5L19.1002 4.8999M22 12H21M18 12C18 15.3137 15.3137 18 12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12Z"/>
-        </svg>
-        `,
-        title: "Problem Solving",
-        description:
-          "Overcame roadblocks by quickly diagnosing bugs and implementing "
-          + "fixes that arose in testing and on launch."
+        icon: "x",
+        title: "x",
+        description:"x"
       }
     ]
   }
